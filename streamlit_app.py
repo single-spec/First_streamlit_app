d503e9ee0f8b71c57c6e61581948dbd74e9f0071
Sonal Ingle <68352812+single-spec@users.noreply.github.com>
2022-04-26 17:07:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,12 @@
 import streamlit
 streamlit.title('My Pinturas bloom with spring')
+
+streamlit.header('Painting Menu - Different Available Art types')
+streamlit.text('Oil painting')
+streamlit.text('Acrylic painting')
+streamlit.text('Abstract Art')
+streamlit.text('Worli Art')
+streamlit.text('Doodles')
+streamlit.text('Calligraphy')
+streamlit.text('Caricature Art')
+streamlit.text('Sketching')
