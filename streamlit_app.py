62985aee45cf065b5cdb699510a7c0ebe2fd5365
Sonal Ingle <68352812+single-spec@users.noreply.github.com>
2022-04-28 21:49:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,10 @@
 import streamlit
-streamlit.title('My Pinturas bloom with spring')
+streamlit.title('My Parents new healthy Diner!')
 
-streamlit.header('Painting Menu - Different Available Art types')
-streamlit.text('Oil painting')
-streamlit.text('Acrylic painting')
-streamlit.text('Abstract Art')
-streamlit.text('Worli Art')
-streamlit.text('Doodles')
-streamlit.text('Calligraphy')
-streamlit.text('Caricature Art')
-streamlit.text('Sketching')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥤🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
