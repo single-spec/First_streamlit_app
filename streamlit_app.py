7b8daac8071badd5b9a5561ccf64bc26df8cbded
Sonal Ingle <68352812+single-spec@users.noreply.github.com>
2022-04-30 22:05:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,11 +31,13 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_ch
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
-#snowflake connector python
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+#snowflake connector python [having issues connecting to connector]
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#--my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+#my_cur.execute("SELECT * FROM fruit_load_list")
+#my_data_row = my_cur.fetchone()
+#--streamlit.text("Hello from Snowflake:")
+#streamlit.header("The fruit load list contains:")
+#streamlit.dataframe(my_data_row)
 
